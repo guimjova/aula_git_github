39675f89fd05e476f98c8c160378754c98236a2f
Guilherme Mariano Jovanelli <guilhermemjovanelli@gmail.com>
2025-10-27 11:27:49 -0300

    Correção de erros no código

--- primeiraleideohm.py
+++ primeiraleideohm.py
@@ -5,16 +5,18 @@ def calc(choose):
     if choose == 1:
         R = int(input("Entre com o valor de resistência (em kΩ): \n\n"))
         I = int(input("\n\nEntre com o valor de corrente (em mA): \n\n"))
-        print(f"\n\nO valor da tensão elétrica é igual a {(R*1000)*(I/1000)}.\n\n")
+        print(f"\n\nO valor da tensão elétrica é igual a {(R*1000)*(I/1000)}V.\n\n")
     elif choose == 2:
         V = int(input("Entre com o valor de tensão (em V): \n\n"))
         I = int(input("\n\nEntre com o valor de corrente (em mA): \n\n"))
-        print(f"\n\nO valor da tensão elétrica é igual a {V/(I/1000)}.\n\n")
+        print(f"\n\nO valor da resistência elétrica é igual a {V/(I/1000)}Ω.\n\n")
     elif choose == 3:
         V = int(input("Entre com o valor de tensão (em V): \n\n"))
         R = int(input("\n\nEntre com o valor de corrente (em kΩ): \n\n"))
-        print(f"\n\nO valor da tensão elétrica é igual a {V/(R*1000)}.\n\n")
+        print(f"\n\nO valor da corrente elétrica é igual a {V/(R*1000)}A.\n\n")
     else:
         print("ERRO: Entrada inválida!")
+
+calc(choose)
 
 print("Desenvolvido por Guilherme Mariano Jovanelli - Engenharia de Controle e Automação - Turma A - SENAI 1.23")
