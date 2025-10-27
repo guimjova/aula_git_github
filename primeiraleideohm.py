b0d47cb84237fec8148a9a9ba2395233b86a7833
Guilherme Mariano Jovanelli <guilhermemjovanelli@gmail.com>
2025-10-27 11:22:15 -0300

    Criação do código em Python

---
+++ primeiraleideohm.py
@@ -0,0 +1,20 @@
+print("Bem-vindo a nova calculadora da Lei de Ohm em Python!")
+choose = int(input("Qual grandeza você gostaria de calcular?\n\n(1: Tensão | 2: Resistência | 3: Corrente)\n\n"))
+
+def calc(choose):
+    if choose == 1:
+        R = int(input("Entre com o valor de resistência (em kΩ): \n\n"))
+        I = int(input("\n\nEntre com o valor de corrente (em mA): \n\n"))
+        print(f"\n\nO valor da tensão elétrica é igual a {(R*1000)*(I/1000)}.\n\n")
+    elif choose == 2:
+        V = int(input("Entre com o valor de tensão (em V): \n\n"))
+        I = int(input("\n\nEntre com o valor de corrente (em mA): \n\n"))
+        print(f"\n\nO valor da tensão elétrica é igual a {V/(I/1000)}.\n\n")
+    elif choose == 3:
+        V = int(input("Entre com o valor de tensão (em V): \n\n"))
+        R = int(input("\n\nEntre com o valor de corrente (em kΩ): \n\n"))
+        print(f"\n\nO valor da tensão elétrica é igual a {V/(R*1000)}.\n\n")
+    else:
+        print("ERRO: Entrada inválida!")
+
+print("Desenvolvido por Guilherme Mariano Jovanelli - Engenharia de Controle e Automação - Turma A - SENAI 1.23")
